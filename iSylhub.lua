ec9e68330927fbca73adc084a7fd308d0d876e5c
iSylvester <feberiyantputra@gmail.com>
2025-10-26 00:03:07 +0700

    Update Games loading URL in iSylhub.lua

--- iSylhub.lua
+++ iSylhub.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/iSylvesterr/iSylHub/refs/heads/main/isyl.lua"))()
 
 local URL = Games[game.PlaceId]
 
