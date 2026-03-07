b888ab572cd337e047aa709a5cbbc7ec2e5eb74a
iSylvester <feberiyantputra@gmail.com>
2026-03-08 02:53:49 +0700

    Update iSylhub.lua

--- iSylhub.lua
+++ iSylhub.lua
@@ -7,12 +7,11 @@ local games = {
 
 local currentPlaceID = game.PlaceId
 local currentUniverseID = game.GameId
-
 local scriptURL = games[currentUniverseID] or games[currentPlaceID]
 
 if scriptURL then
-    print("Meng Hub: Loading script for ID " .. (games[currentUniverseID] and "Universe" or "Place"))
-    loadstring(game:HttpGet(scriptURL))()
+    print("iSylHub: Loading script for ID " .. (games[currentUniverseID] and "Universe" or "Place"));
+    (loadstring or load)(game:HttpGet(scriptURL))()
 else
     local msg = "\nMap not supported yet!\nPlaceId: " .. tostring(currentPlaceID) .. "\nUniverseId: " .. tostring(currentUniverseID)
     game.Players.LocalPlayer:Kick(msg)
