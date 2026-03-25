374e7f05bd770fe79854e4c23a0609e5944701bf
iSylvester <feberiyantputra@gmail.com>
2026-03-26 01:26:44 +0700

    Update iSylhub.lua

--- iSylhub.lua
+++ iSylhub.lua
@@ -1,22 +1,42 @@
 local games = {
-    [5750914919] = "https://raw.githubusercontent.com/iSylvesterr/library/refs/heads/main/Fisch.lua",
-    [16732694052] = "https://raw.githubusercontent.com/iSylvesterr/library/refs/heads/main/Fisch.lua",
+    -- Kamu bisa bebas memasukkan UniverseId (GameId) maupun PlaceId ke sini
+    [5750914919]      = "https://raw.githubusercontent.com/iSylvesterr/library/refs/heads/main/Fisch.lua",
+    [16732694052]     = "https://raw.githubusercontent.com/iSylvesterr/library/refs/heads/main/Fisch.lua",
     [131716211654599] = "https://raw.githubusercontent.com/iSylvesterr/library/refs/heads/main/Fisch.lua",
-    [78632820802305] = "https://raw.githubusercontent.com/iSylvesterr/library/refs/heads/main/GetFish.lua",
+    [78632820802305]  = "https://raw.githubusercontent.com/iSylvesterr/library/refs/heads/main/GetFish.lua",
     [130342654546662] = "https://raw.githubusercontent.com/iSylvesterr/library/refs/heads/main/SambungKata.lua",
     [110369730911937] = "https://raw.githubusercontent.com/iSylvesterr/library/refs/heads/main/cdid.lua",
-    [6701277882] = "https://raw.githubusercontent.com/iSylvesterr/library/refs/heads/main/Fish It.lua",
+    [6701277882]      = "https://raw.githubusercontent.com/iSylvesterr/library/refs/heads/main/Fish It.lua",
 }
+
+-- [IMPROVEMENT 1]: Tunggu hingga UniverseId valid (mencegah bug GameId bernilai 0 di Auto-Execute)
+if game.GameId == 0 then
+    game:GetPropertyChangedSignal("GameId"):Wait()
+end
 
 local currentPlaceID = game.PlaceId
 local currentUniverseID = game.GameId
+
+-- Cek apakah script tersedia untuk UniverseId atau PlaceId
 local scriptURL = games[currentUniverseID] or games[currentPlaceID]
+local isUniverse = (games[currentUniverseID] ~= nil)
 
 if scriptURL then
-    print("iSylHub: Loading script for ID " .. (games[currentUniverseID] and "Universe" or "Place"));
-    (loadstring or load)(game:HttpGet(scriptURL))()
+    -- Print info agar jelas script mana yang terpanggil
+    print("iSylHub: Loading script for " .. (isUniverse and "Universe ID: " .. tostring(currentUniverseID) or "Place ID: " .. tostring(currentPlaceID)))
+    
+    -- [IMPROVEMENT 2]: Menggunakan pcall agar executor tidak crash jika link GitHub mati / error
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(scriptURL))()
+    end)
+
+    if not success then
+        local errorMsg = "iSylHub: Gagal memuat script! Error: " .. tostring(err)
+        warn(errorMsg)
+        game.Players.LocalPlayer:Kick("\n" .. errorMsg)
+    end
 else
-    local msg = "\nMap not supported yet!\nPlaceId: " .. tostring(currentPlaceID) .. "\nUniverseId: " .. tostring(currentUniverseID)
+    local msg = "\n[iSylHub] Map not supported yet!\nPlaceId: " .. tostring(currentPlaceID) .. "\nUniverseId: " .. tostring(currentUniverseID)
     game.Players.LocalPlayer:Kick(msg)
     print(msg)
 end
